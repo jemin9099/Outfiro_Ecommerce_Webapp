16db57090c57a7dc9921bca97c85735bf28a1502
piyushfilestride <piyush.nakrani@filestride.com>
2025-02-14 15:31:35 +0530

    modify router file

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import home from '../views/index.vue'
+import listing from '../views/shop/listing/index.vue'
 import axios from "@/axios"
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,7 +19,8 @@ const router = createRouter({
     {
       path: '/shop/listing',
       name: 'listing',
-      component: () => import('../views/shop/listing/index.vue'),
+      component: listing,
+      // component: () => import('../views/shop/listing/index.vue'),
       meta:{
         title:'Listing',
         requiresAuth: false,
